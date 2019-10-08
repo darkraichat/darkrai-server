f72ba57c7e6fe883ebbc873516431df7e03cafb1
Mohamed Shadab <mohamedshadab78@gmail.com>
2019-10-08 15:58:25 +0530

    refactor: improves the dev experience (#4)
    * refactor: WIP improve dev experience
    
    Signed-off-by: Mohamed Shadab <mohamedshadab78@gmail.com>
    
    * fix: docker dev fixed
    
    Signed-off-by: Mohamed Shadab <mohamedshadab78@gmail.com>

--- extension/src/index.js
+++ extension/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import App from './App'
-import registerServiceWorker from './registerServiceWorker'
+import 'shards-ui/dist/css/shards.min.css'
+import './index.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
-registerServiceWorker()
--- server/src/db.js
+++ server/src/db.js
@@ -1,10 +1,16 @@
 const { MongoClient } = require('mongodb')
+require('dotenv').config()
 
-const mongo_url =
-  'mongodb+srv://hit:mongo123@cluster0-gd4cn.mongodb.net/test?retryWrites=true&w=majority'
+const mongo_url = process.env.DB_URL || ''
 
 module.exports.connectdb = dbName => {
-  return MongoClient.connect(mongo_url, { useNewUrlParser: true }).then(
-    client => client.db(dbName)
-  )
+  return MongoClient.connect(mongo_url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+    .then(client => {
+      console.log('Connected to MongoDB...')
+      return client.db(dbName)
+    })
+    .catch(err => console.log('MongoDB Error:\n', err))
 }
