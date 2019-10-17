36b4aa2fe25942dcd6adb9f218a54f09b9bdf8cd
smit2k14 <smir2k14@gmail.com>
2019-10-17 20:14:02 +0530

    chore: changed the input length of model

--- python/model.py
+++ python/model.py
@@ -5,9 +5,9 @@ from keras.layers.embeddings import Embedding
 class Model():
     def __init__(self):
         self.model = self.create_model()
-    def create_model(self, vocab_size = 100000, embedding_size = 100, input_length = 150):
+    def create_model(self, vocab_size = 100000, embedding_size = 100, input_length = 50):
         model = Sequential()
-        model.add(Embedding(vocab_size, embedding_size, input_length=150))
+        model.add(Embedding(vocab_size, embedding_size, input_length=input_length))
         model.add(LSTM(100, dropout=0.2, recurrent_dropout=0.2))
         model.add(Dense(2, activation='sigmoid'))
         model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
